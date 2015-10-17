4b3b55a2b882b0022f461c5c898ac9a00de9dbde
Piotr Waszkiewicz <Waszka23@gmail.com>
2015-10-17 14:38:48 +0200

    Created WinForms project. Created Automaton and State classes.

---
+++ TAIO/Automaton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAIO
+{
+    /// <summary>
+    /// Class representing finite, deterministic automaton.
+    /// </summary>
+    class Automaton
+    {
+        private List<State> states;
+
+        /// <summary>
+        /// Returns number of active state after computations.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public int GetFinalState(String word)
+        {
+            State currentState = states[0];
+            for (int i = 0; i < states.Count; i++)
+                currentState = states.ElementAt(currentState.GetNextStateNumber(word[i]));
+            return states.IndexOf(currentState);
+        }
+    }
+}
---
+++ TAIO/State.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAIO
+{
+    /// <summary>
+    /// Class representing automaton state during computation.
+    /// </summary>
+    class State
+    {
+        private bool isAccepting;
+        private Dictionary<char, int> controlFunction;
+
+        /// <summary>
+        /// Returns next state number according to controlFunction associated with this state.
+        /// </summary>
+        /// <param name="letter"></param>
+        /// <returns></returns>
+        public int GetNextStateNumber(char letter)
+        {
+            int nextStateNumber;
+            controlFunction.TryGetValue(letter, out nextStateNumber);
+            return nextStateNumber;
+        }
+    }
+}
