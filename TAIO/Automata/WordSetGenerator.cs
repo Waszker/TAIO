53abb980dbe386d979f770bb98e67a4b2f08ff2a
Sylwia Nowak <Sylwia.Nowak@connectis.pl>
2015-10-20 19:36:39 +0200

    * create test project and word generator

---
+++ TAIO/Automata/WordSetGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TAIO.Automata
+{
+    public class WordSetGenerator
+    {
+        private char[] _letters;
+        private List<String> _words; 
+
+        public WordSetGenerator(char[] letters)
+        {
+            _letters = letters;
+            _words = new List<string>();
+        }
+
+        public void GenerateWords(int maxLength)
+        {
+            _words = _letters.Select(letter => letter.ToString()).ToList();
+            var produceWithRecursion = ProduceWithRecursion(_words);
+            foreach (var word in produceWithRecursion)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        private void GenerateWordsSpecifiedLength(int length)
+        {
+            
+        }
+
+        private IEnumerable<List<String>> ProduceWithRecursion(List<String> allValues)
+        {
+            for (var i = 0; i < (1 << allValues.Count); i++)
+            {
+                yield return ConstructSetFromBits(i).Select(n => allValues[n]).ToList();
+            }
+        }
+
+        private IEnumerable<int> ConstructSetFromBits(int i)
+        {
+            var n = 0;
+            for (; i != 0; i /= 2)
+            {
+                if ((i & 1) != 0) yield return n;
+                n++;
+            }
+        }
+    }
+}
---
+++ TAIOTest/WordSetGeneratorTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TAIO;
+using TAIO.Automata;
+
+namespace TAIOTest
+{
+    public class WordSetGeneratorTest
+    {
+        [Test]
+        public void GenerateVariationsTest()
+        {
+            char[] letters = {'A', 'B', 'C'};
+            WordSetGenerator wordSetGenerator = new WordSetGenerator(letters);
+            wordSetGenerator.GenerateWords(10);
+
+        }
+    }
+}
+
