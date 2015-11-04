d38c04f636abfe773bac5f99c6305f3353243a8a
Anna Zawadzka <Anna Zawadzka>
2015-11-04 16:19:09 +0100

    Size of the window with graph image is fitted to the picture size

--- TAIO/PictureWindow.cs
+++ TAIO/PictureWindow.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 
 namespace TAIO
 {
+    /// <summary>
+    /// Window used to show input and output graph
+    /// </summary>
     public partial class PictureWindow : Form
     {
         public PictureWindow()
@@ -17,11 +20,18 @@ namespace TAIO
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Takes name of window and name of automaton (to get the right image)
+        /// </summary>
+        /// <param name="windowName"></param>
+        /// <param name="automatonName"></param>
         public PictureWindow(string windowName, string automatonName)
         {
             InitializeComponent();
-
+            int diffw = this.Width - pictureBox1.Width;
+            int diffh = this.Height - pictureBox1.Height;
             pictureBox1.Image = new Bitmap(automatonName);
+            this.Size = new System.Drawing.Size(pictureBox1.Image.Width+diffw, pictureBox1.Image.Height + diffh);
             pictureBox1.Dock = DockStyle.Fill;
             this.Text = windowName;
         }
