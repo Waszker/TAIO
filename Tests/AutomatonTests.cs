f2a36e81b949561046e96add05f54b3b1dc8646a
sylwia.nowak <sylwia.nowak@cybercom.com>
2015-12-12 11:24:09 +0100

    * basic automaton test

--- Tests/AutomatonTests.cs
+++ Tests/AutomatonTests.cs
@@ -7,17 +7,44 @@ namespace Tests
     [TestClass]
     public class AutomatonTests
     {
+        /// <summary>
+        /// Testing constructing automaton
+        /// </summary>
         [TestMethod]
         public void AutomatonConstructionFromPositionObjectTest()
         {
             // Arrange
-            Position position = new Position(2, 3, 0) {OnePositions = new[,] {{1, 2, 1}, {2, 2, 1}}};
+            Position position = new Position(2, 3, 0) { OnePositions = new[,] { { 1, 2, 1 }, { 2, 2, 1 } } };
 
             // Act
             Automaton automaton = new Automaton(position);
 
             // Assert
             Assert.AreEqual(automaton.GetFinalState("101"), 2);
+        }
+
+        [TestMethod()]
+        public void AutomatonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AutomatonTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetFinalStateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGraphTest()
+        {
+            Assert.Fail();
         }
     }
 }
